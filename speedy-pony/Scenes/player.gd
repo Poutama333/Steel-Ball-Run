c5c1dfc3d55f1cb41f544714518a2821fbf2a291
Poutama333 <lintolareihanat@wsc.school.nz>
2026-09-25 12:59:02 +1200

    25-09-26
    Added acceleration and deceleration, added first obstacle

--- speedy-pony/Scenes/player.gd
+++ speedy-pony/Scenes/player.gd
@@ -1,25 +1,23 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
-
-
-func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+func _physics_process(delta):
+	velocity = Vector2()
+	
+	var speed = 250
+	
+	
+	if Input.is_key_pressed(KEY_LEFT):
+		speed = 100
+	if Input.is_key_pressed(KEY_RIGHT):
+		speed = 350
+	if Input.is_key_pressed(KEY_DOWN):
+		velocity.y += 250
+	if Input.is_key_pressed(KEY_UP):
+		velocity.y -= 250
+	
+	velocity.x = speed	
+	
 	move_and_slide()
+	
+	
